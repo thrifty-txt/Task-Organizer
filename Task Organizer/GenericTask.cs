48c919e0790b541bf73977ee2d3715f44b17d8d7
Ali <Ali@RDPWINDOWS>
2021-05-28 10:59:59 -0400

    Add project files.

---
+++ Task Organizer/GenericTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Organizer {
+    class GenericTask {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public GenericTask Parent { get; set; }
+        public List<GenericTask> Children { get; set; }
+    }
+}
